da08d3810da17f828948984058667df989e9c10e
MoroniAg <moroniaguilera1993@gmail.com>
2022-05-13 12:09:57 -0500

    fix author

---
+++ src/main/java/org/cumorah_center/archetype/CrudArchetype.java
@@ -0,0 +1,118 @@
+package org.cumorah_center.archetype;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.UUID;
+
+/**
+ * Created by Gabriel Aguilera on 03-05-2022
+ */
+public abstract class CrudArchetype<T extends PanacheEntityBase, S> {
+
+    private static final Logger LOGGER = LogManager.getLogger(CrudArchetype.class.getName());
+
+    /**
+     * Method for persist entity in database
+     * @param t
+     * @return Response
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response create(T t) {
+        String trace = UUID.randomUUID().toString();
+        LOGGER.log(Level.INFO, "({}) Persist entity: {}", trace, t);
+        if (t != null) {
+            t.persist();
+            if (t.isPersistent()) {
+                LOGGER.log(Level.INFO, "({}) Entity status persist : {}", trace, t.isPersistent());
+                return Response.ok(t).build();
+            } else {
+                LOGGER.log(Level.WARN, "({}) Entity not persist", trace);
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+        }
+        LOGGER.log(Level.ERROR, "({}) Entity not exist", trace);
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+    }
+
+    /**
+     * Method for search entity by uuid
+     * @param s
+     * @return
+     */
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response read(S s) {
+        String trace = UUID.randomUUID().toString();
+        LOGGER.log(Level.INFO, "({}) Obtain entity by uuid: {}", trace, s);
+        if (s != null) {
+            T t = T.findById(s);
+            if (t != null) {
+                LOGGER.log(Level.DEBUG, "({}) Entity found: {}", trace, t);
+                return Response.ok(t).build();
+            } else {
+                return Response.status(Response.Status.BAD_REQUEST).build();
+            }
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
+    /**
+     * Method for update entity by uuid
+     * @param t
+     * @param uuid
+     * @return
+     */
+    @Path("/{uuid}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response update(T t, @PathParam("uuid") S uuid) {
+        String trace = UUID.randomUUID().toString();
+        LOGGER.log(Level.INFO, "({}) Update entity: {} by id: {}", trace, t, uuid);
+        if (t != null) {
+            t.persist();
+            return Response.ok(t).build();
+        } else {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+    /**
+     * Method for delete logical or physical entity
+     * @param t
+     * @param uuid
+     * @return
+     */
+    @Path("/{uuid}")
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Response delete(T t, @PathParam("uuid") S uuid) {
+        String trace = UUID.randomUUID().toString();
+        LOGGER.log(Level.INFO, "({}) Update entity: {} by id: {}", trace, t, uuid);
+        if (t != null) {
+            t.delete();
+            return Response.ok(t).build();
+        } else {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+
+}
+
+
